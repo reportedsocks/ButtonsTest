2eb59093dbf5bb18de47bff7c18c927fed15ca43
reportedsocks <dreeantsyferov@gmail.com>
2020-02-25 22:25:33 +0200

    Initial commit

---
+++ app/src/main/java/com/reportedsocks/buttonstest/MainActivity.java
@@ -0,0 +1,103 @@
+package com.reportedsocks.buttonstest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView[] imageViews;
+    private int[] wasImageClickedArray;
+    private int[] images;
+    private int[] imagesClicked;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wasImageClickedArray = new int[]{0,0,0,0,0};
+
+        images = new int[5];
+        images[0] = R.drawable.tb_ic_menu;
+        images[1] = R.drawable.tb_ic_shop;
+        images[2] = R.drawable.tb_ic_pause;
+        images[3] = R.drawable.tb_ic_normal;
+        images[4] = R.drawable.tb_ic_accelerated;
+
+        imagesClicked = new int[5];
+        imagesClicked[0] = R.drawable.tb_ic_menu_clicked;
+        imagesClicked[1] = R.drawable.tb_ic_shop_clicked;
+        imagesClicked[2] = R.drawable.tb_ic_pause_clicked;
+        imagesClicked[3] = R.drawable.tb_ic_normal_clicked;
+        imagesClicked[4] = R.drawable.tb_ic_accelerated_clicked;
+
+        imageViews = new ImageView[5];
+        imageViews[0] = findViewById(R.id.menu_image);
+        imageViews[1] = findViewById(R.id.shop_image);
+        imageViews[2] = findViewById(R.id.pause_image);
+        imageViews[3] = findViewById(R.id.play_image);
+        imageViews[4] = findViewById(R.id.accelerated_image);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int[] viewLocationArray = new int[2];
+        int eventPadTouch = event.getAction();
+        // get coordinates of user click
+        float eventX = event.getX();
+        float eventY = event.getY();
+
+        switch (eventPadTouch) {
+
+            case MotionEvent.ACTION_DOWN:
+                for( int i = 0; i < imageViews.length; i++ ){
+
+                    // get coordinates of ImageView position
+                    // then rest them from coordinates of user click
+                    // this gives coordinates of click on Image itself
+                    imageViews[i].getLocationInWindow(viewLocationArray);
+                    float X = eventX - viewLocationArray[0];
+                    float Y = eventY - viewLocationArray[1];
+
+                    //decode bitmap of image
+                    Bitmap bitmap = BitmapFactory.decodeResource(getResources(), images[i]);
+
+                    if ( X >= 0 & Y >= 0
+                            & X < bitmap.getWidth() & Y < bitmap.getHeight() ){
+                        // if user didn't click on transparent part, pixel won't be zero
+                        if ( bitmap.getPixel((int) X, (int) Y) != 0) {
+                            int pos = i+1;
+                            Log.d("MyLogs", "clicked button " + pos);
+                            Toast.makeText(this,
+                                    "Button clicked: " + pos, Toast.LENGTH_SHORT).show();
+                            imageViews[i].setImageResource(imagesClicked[i]);
+                            wasImageClickedArray[i] = 1;
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            case MotionEvent.ACTION_UP:
+                // if any ImageView was marked as clicked, set normal image to it
+                for( int i = 0; i < wasImageClickedArray.length; i++ ){
+                    if( wasImageClickedArray[i] == 1 ){
+                        imageViews[i].setImageResource(images[i]);
+                        wasImageClickedArray[i] = 0;
+                        return true;
+                    }
+                }
+                return true;
+            default:
+                return false;
+        }
+    }
+}
